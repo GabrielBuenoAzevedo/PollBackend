8aebdd2fa054e86e2e3e07c4df6a1897fb0d125d
Gabriel BuenoAzevedo <bielalgumacoisa@gmail.com>
2020-06-18 10:25:53 -0300

    [UPT] Verification for userId existence in database.

--- app/middlewares/user.middleware.js
+++ app/middlewares/user.middleware.js
@@ -2,10 +2,13 @@ const { User } = require('../models')
 
 exports.getTokenUser = async (req, res, next) => {
   if (!req.userId){
-    res.status(505).send({error: 'UserId não encontrado no token oferecido.'})
+    res.status(505).send({error: 'UserId not found inside given token.'})
     return;
   }
   const user = await User.findById(req.userId).exec();
+  if (user === null) {
+    res.status(505).send({error: 'User not found'})
+  }
   req.dbData.user = user;
   next();
 }
